3c46c58f529f83c9491852dd7515b4f9a336d9f3
Bernedotcom <valerian.pyckaert@ynov.com>
2017-12-01 16:41:14 +0100

    Add DjangoRestFramework + Models

--- api/urls.py
+++ api/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('bobby_unknowns_battle_cards.urls'))
+    url(r'^', include('app.urls'))
 ]
--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    name = models.CharField(max_length=50)
+    cost  = models.IntegerField(default = 1)
+
+class Deck(models.Model):
+    name = models.CharField(max_length=50)
+    cards = models.ForeignKey(Card)
+
+class Player(models.Model):
+    name = models.CharField(max_length=5)
+    health = models.IntegerField(default = 30)
+    current_deck = models.ForeignKey(Deck)
+    mana_cristals = models.IntegerField(default = 1)
+
+class MonsterCard(Card):
+    health = models.IntegerField(default = 1)
+    strengh  = models.IntegerField(default = 1)
+
+class SpellCard(Card):
+    effect = models.CharField(max_length = 100)
