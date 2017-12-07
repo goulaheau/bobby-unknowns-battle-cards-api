f8d5d82767997fd7eee7cf4cfddc4a53846fa94a
BerneDotcom <valerian.pyckaert@ynov.com>
2017-12-07 09:23:10 +0100

    Add to_string to models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app.models import Deck,Player,MonsterCard,SpellCard
 
 # Register your models here.
+admin.site.register(Deck);
+admin.site.register(Player);
+admin.site.register(MonsterCard);
+admin.site.register(SpellCard);
--- app/models.py
+++ app/models.py
@@ -5,15 +5,24 @@ class Card(models.Model):
     name = models.CharField(max_length = 50)
     cost = models.IntegerField(default = 1)
 
+    def __str__(self):
+        return self.name
+
 class Deck(models.Model):
     name = models.CharField(max_length = 50)
     cards = models.ForeignKey(Card, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
 
 class Player(models.Model):
     name = models.CharField(max_length = 5)
     health = models.IntegerField(default = 30)
     current_deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
     mana_cristals = models.IntegerField(default = 1)
+
+    def __str__(self):
+        return self.name
 
 class MonsterCard(Card):
     health = models.IntegerField(default = 1)
