a9f4be37d060e7aef57389948b23cdb005579744
BerneDotcom <valerian.pyckaert@ynov.com>
2017-12-07 08:57:17 +0100

    Fix migrate

--- app/models.py
+++ app/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Card(models.Model):
-    name = models.CharField(max_length=50)
-    cost  = models.IntegerField(default = 1)
+    name = models.CharField(max_length = 50)
+    cost = models.IntegerField(default = 1)
 
 class Deck(models.Model):
-    name = models.CharField(max_length=50)
-    cards = models.ForeignKey(Card)
+    name = models.CharField(max_length = 50)
+    cards = models.ForeignKey(Card, on_delete=models.DO_NOTHING)
 
 class Player(models.Model):
-    name = models.CharField(max_length=5)
+    name = models.CharField(max_length = 5)
     health = models.IntegerField(default = 30)
-    current_deck = models.ForeignKey(Deck)
+    current_deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
     mana_cristals = models.IntegerField(default = 1)
 
 class MonsterCard(Card):
