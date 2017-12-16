1db59ac648fcf845b34224bd6af843794780481d
Enzo Cornand <goulaheau@gmail.com>
2017-12-16 13:43:02 +0100

    Sockets

--- app/consumers.py
+++ app/consumers.py
@@ -1,17 +1,20 @@
 from channels import Group
 
 
-def ws_connect(message):
+def ws_connect(message, game_id):
+    print(game_id)
     message.reply_channel.send({'accept': True})
     Group('users').add(message.reply_channel)
 
 
-def ws_receive(message):
+def ws_receive(message, game_id):
     print(message.content['text'])
+    print(game_id)
     Group('users').send({
         'text': message.content['text']
     })
 
 
-def ws_disconnect(message):
+def ws_disconnect(message, game_id):
+    print(game_id)
     Group('users').discard(message.reply_channel)
