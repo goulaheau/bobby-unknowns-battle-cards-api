1933218b53199bfeb13f8fff16599163bef8d4a7
Enzo Cornand <goulaheau@gmail.com>
2018-01-12 11:35:26 +0100

    Merge

--- app/admin.py
+++ app/admin.py
@@ -1,21 +1,9 @@
 from app.models import *
 
 
-
-# Register your models here.
-<<<<<<< HEAD
-admin.site.register(User, UserAdmin)
-admin.site.register(CardType)
-admin.site.register(CardEffect)
-admin.site.register(Card)
-admin.site.register(Deck)
-=======
 admin.site.register(Deck, DeckAdmin)
 admin.site.register(User, UserAdmin)
-# admin.site.register(User)
 admin.site.register(Card, CardAdmin)
 admin.site.register(CardEffect, CardEffectAdmin)
 admin.site.register(CardType)
 admin.site.register(GameLog, GameLogAdmin)
-
->>>>>>> f82567a8d9f47442be8e29928f3ed2abaaceaa85
