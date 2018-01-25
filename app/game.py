70707ab2515e60b2f5268a637b6dfa7c94fef572
Enzo Cornand <goulaheau@gmail.com>
2018-01-25 09:57:20 +0100

    Attack

--- app/game.py
+++ app/game.py
@@ -3,6 +3,18 @@ import random
 from app.models import Card, CardValues
 
 
+def action_switcher(game, user, action, payload):
+    switcher = {
+        'init': action_init,
+        'play_card': action_play_card,
+        'attack': action_attack
+    }
+
+    func = switcher.get(action)
+
+    return func(game, user, payload)
+
+
 def action_init(game, user, payload):
     player_turn = random.randint(0, 1)
     game.player_turn = [game.owner, game.opponent][player_turn]
@@ -34,17 +46,21 @@ def action_init(game, user, payload):
 
     return {
         'action': 'init',
-        'payload': payload
+        'payload': {
+            'success': True
+        }
     }
 
 
 def action_play_card(game, user, payload):
+    global card_to_play
     success = True
-    card_to_play = Card.objects.get(id=payload['card_to_play'])
 
     if user.id == game.owner.id:
         try:
-            game.owner_hand_cards.get(id=card_to_play.id)
+            card_to_play = game.owner_hand_cards.get(
+                id=payload['card_to_play']
+            )
         except Card.DoesNotExist:
             success = False
 
@@ -64,7 +80,9 @@ def action_play_card(game, user, payload):
             game.save()
     else:
         try:
-            game.opponent_hand_cards.get(id=card_to_play.id)
+            card_to_play = game.opponent_hand_cards.get(
+                id=payload['card_to_play']
+            )
         except Card.DoesNotExist:
             success = False
 
@@ -93,19 +111,75 @@ def action_play_card(game, user, payload):
     }
 
 
+def action_attack(game, user, payload):
+    global attacker, victim
+    success = True
+
+    if user.id == game.owner.id:
+        try:
+            attacker = game.owner_board_cards.get(id=payload['attacker'])
+            victim = game.opponent_board_cards.get(id=payload['victim'])
+        except CardValues.DoesNotExist:
+            success = False
+
+        if success:
+            victim.health = victim.health - attacker.strengh
+            attacker.health = attacker.health - victim.strengh
+
+            if attacker.health <= 0:
+                game.owner_board_cards.remove(attacker)
+                game.owner_graveyard_cards.add(attacker.card)
+                game.save()
+                attacker.delete()
+            else:
+                attacker.save()
+            if victim.health <= 0:
+                game.opponent_board_cards.remove(victim)
+                game.opponent_graveyard_cards.add(victim.card)
+                game.save()
+                victim.delete()
+            else:
+                victim.save()
+    else:
+        try:
+            attacker = game.opponent_board_cards.get(id=payload['attacker'])
+            victim = game.owner_board_cards.get(id=payload['victim'])
+        except CardValues.DoesNotExist:
+            success = False
+
+        if success:
+            victim.health = victim.health - attacker.strengh
+            attacker.health = attacker.health - victim.strengh
+
+            if attacker.health <= 0:
+                game.opponent_board_cards.remove(attacker)
+                game.opponent_graveyard_cards.add(attacker.card)
+                game.save()
+                attacker.delete()
+            else:
+                attacker.save()
+            if victim.health <= 0:
+                game.owner_board_cards.remove(victim)
+                game.owner_graveyard_cards.add(victim.card)
+                game.save()
+                victim.delete()
+            else:
+                victim.save()
+
+    return {
+        'action': 'attack',
+        'payload': {
+            'emitter': user.id,
+            'success': success,
+            'attacker': payload['attacker'],
+            'victim': payload['victim'],
+        }
+    }
+
+
 def draw_card(deck_cards, hand_cards):
     card_to_draw = random.randint(0, len(deck_cards.all()) - 1)
 
     hand_cards.add(deck_cards.all()[card_to_draw])
     deck_cards.remove(deck_cards.all()[card_to_draw])
 
-
-def action_switcher(game, user, action, payload):
-    switcher = {
-        'init': action_init,
-        'play_card': action_play_card
-    }
-
-    func = switcher.get(action)
-
-    return func(game, user, payload)
